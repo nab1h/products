5f01f06a7b33ae6996a64736a5fadd1e78eecf26
nab1h <nab1h@outlook.com>
2026-01-15 19:20:25 +0200

    end form

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,41 @@ import ProductCard from "./components/ProductCard/ProductCard";
 import Modal from "../src/components/ui/Modal";
 import Button from "../src/components/ui/Button";
 import Input from "../src/components/ui/Input";
-import { formInputsList, productList } from "./data";
-import { useState } from "react";
+import { formInputsList, productList, type IProduct } from "./data";
+import { useState, type ChangeEvent } from "react";
+import type { FormEvent } from "react";
+
+
+
 
 function App() {
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
+  const [product, setProduct] = useState<IProduct>({
+    title: "",
+    description: "",
+    price: "",
+    imageURL: "",
+  });
+
+  const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name,value } = e.target;
+    setProduct({
+      ...product,
+      [name]: value,
+    });
+  };
+  const onsubmitForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setProduct({
+      title: "",
+      description: "",
+      price: "",
+      imageURL: "",
+    });
+  };
   // -----------
   const productListInput = formInputsList.map((input) => (
     <div className="grid grid-cols-1 space-y-2 my-4">
@@ -17,9 +45,13 @@ function App() {
         className="border-2 border-indigo-400 rounded-sm shadow-md focus:outline-none foucs:ring-2 focus:ring-indigo-500"
         type={input.type}
         name={input.name}
+        value={product[input.name as keyof IProduct]}
+        onChange={onchangeHandler}
       />
     </div>
   ));
+  // ======================================
+
   // ======================================
   const [isOpen, setIsOpen] = useState(true);
 
@@ -38,11 +70,15 @@ function App() {
           {renderProductList}
         </div>
         <Modal isOpen={isOpen} close={close}>
-          {productListInput}
-          <div className="flex space-x-3">
-            <Button className="bg-indigo-700">submit</Button>
-            <Button className="bg-red-700">close</Button>
-          </div>
+          <form onSubmit={onsubmitForm}>
+            {productListInput}
+            <div className="flex space-x-3">
+              <Button type="submit" className="bg-indigo-700">
+                submit
+              </Button>
+              <Button className="bg-red-700">close</Button>
+            </div>
+          </form>
         </Modal>
       </div>
     </>
